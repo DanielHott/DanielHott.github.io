e52f66a9ed6848eacc734a15b12ebf2a25b05d99
Daniel Hott <daniel_hott@outlook.com>
2021-11-04 20:06:43 -0300

    Adicionando teste

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from './components/main';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Main />
     </BrowserRouter>
