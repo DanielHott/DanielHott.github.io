574d144197ea9c14a010c4cec2ec74a22a9e1905
Daniel Hott <daniel_hott@outlook.com>
2021-12-01 20:54:14 -0300

    Adicionando informaçoes de contato

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
@@ -8,11 +8,11 @@ import React from 'react';
 class App extends React.Component {
     render() {
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Header />
         <Main />
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
