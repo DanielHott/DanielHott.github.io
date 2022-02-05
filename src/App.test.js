2b59b67e2b4ca056ab46348039374156916f7627
Daniel Hott <daniel_hott@outlook.com>
2022-02-05 16:40:10 -0300

    Adicionando build

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Verifica se tem titulo', () => {
-  render(<App />);
-  const titulo = screen.getByText(/Daniel Hott/i);
-  expect(titulo).toBeInTheDocument();
-});
